83868a4291d392d2d1968b81faf21f08107338a6
HubertStefanski <hubertstefanski@gmail.com>
2019-11-07 23:09:36 +0000

    add cars.json

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,22 +1,13 @@
-import { withRouter } from 'react-router-dom';
 import React, { Component, Fragment } from 'react';
 
 
-class Home extends Component {
+export default class Home extends Component {
     render() {
         return (
             <Fragment>
                 <div className="row">
                     <div className="col-md-6 offset-3">
-                        <h1>Frequently Asked Questions </h1>
-                        <h1> Who are we?</h1>
-                        <h1> We are carlet, a car sharing startup aiming to make renting cars easier by bringing you to others who are willing to rent their vehicle out</h1>
-                        <h1> Why choose us?</h1>
-                        <h1> We link you directly with renters, you have full clarity over the whole process and our full support throughout</h1>
-                        <h1> I just found a deal on site X which is better than what you have</h1>
-                        <h1> Carlet doesn't set the price for the rentals, we only provide users with a bridge between them and renters, our service charges are amongst the lowest in the industry</h1>
-                        <h1> Why list your car?</h1>
-                        <h1> The answer is simple,since our service charges are minimal, you are able to get more for less, while still having all the benefits of any other company!</h1>
+                        <h1> Welcome to Carlet your one stop shop for all your car letting needs</h1>
                     </div>
                 </div>
 
@@ -24,4 +15,4 @@ class Home extends Component {
         );
     }
 }
-export default withRouter(Home);
+
